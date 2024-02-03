98bd7e4485be4fcc009549705aa32194cba7f6d3
vs45 <bringfirehjake88@gmail.com>
2024-02-03 12:53:40 +0100

    class work

---
+++ classwork.py
@@ -0,0 +1,9 @@
+numb1=0
+numb2=1
+try:
+    numb1=int(input("enter your first number: "))
+    numb2=int(input("enter your second number: "))
+except ValueError as ve:
+    print("Wrong value: ",ve)
+product=numb1+numb2
+print(product)
